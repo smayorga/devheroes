62c326ab012ce387459faf30437856607ee9d3a7
Sandra Mayorga <samayorg@gmail.com>
2016-05-23 21:26:32 -0500

    Adicionando pagina para el taller

---
+++ js/heroes.js
@@ -0,0 +1,5 @@
+$(function() {
+	//$("#portfolioModal1").load("heroes/b.html"); 
+	$("#portfolioModal1").load("http://www.cafetosoftware.com"); 
+	//$("#portfolioModal1").innerHTML='<object type="type/html" data="heroes/b.html" ></object>';
+}); 
